97f643336ffd43e377e01bef73c72e97515425fb
khoidm2004 <khoidm2004@gmail.com>
2023-11-27 21:18:44 +0200

    Navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { motion, useScroll } from "framer-motion";
+import Headroom from "react-headroom";
 import "./App.css";
 
 import About from "./Component/About/About";
@@ -14,11 +15,13 @@ function App() {
   return (
     <>
       <div>
+        <Headroom downTolerance={100}>
+          <NavBar />
+        </Headroom>
         <motion.div
           className="progress_bar"
           style={{ scaleX: scrollYProgress }}
         />
-        <NavBar />
         <Home />
         <About />
         <Resume />
