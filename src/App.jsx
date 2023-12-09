5810f8de8b57d36195f966d56ccc82bd0c6eb075
khoidm2004 <khoidm2004@gmail.com>
2023-12-09 22:24:52 +0200

    Create loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { motion, useScroll } from "framer-motion";
 import Headroom from "react-headroom";
-import "./App.css";
+import React, { Suspense } from "react";
 
+import "./App.css";
 import About from "./Component/About/About";
 import Contact from "./Component/Contact/Contact";
 import Home from "./Component/Home/Home";
@@ -12,21 +13,30 @@ import Resume from "./Component/Resume/Resume";
 function App() {
   const { scrollYProgress } = useScroll();
 
+  const Home1 = React.lazy(() => import("./Component/Home/Home"));
+  const NavBar1 = React.lazy(() => import("./Component/NavBar/NavBar"));
+  const About1 = React.lazy(() => import("./Component/About/About"));
+  const Resume1 = React.lazy(() => import("./Component/Resume/Resume"));
+  const Projects1 = React.lazy(() => import("./Component/Projects/Projects"));
+  const Contact1 = React.lazy(() => import("./Component/Contact/Contact"));
+
   return (
     <>
       <div>
-        <Headroom downTolerance={100}>
-          <NavBar />
-        </Headroom>
-        <motion.div
-          className="progress_bar"
-          style={{ scaleX: scrollYProgress }}
-        />
-        <Home />
-        <About />
-        <Resume />
-        <Projects />
-        <Contact />
+        <Suspense>
+          <Headroom downTolerance={100}>
+            <NavBar1 />
+          </Headroom>
+          <motion.div
+            className="progress_bar"
+            style={{ scaleX: scrollYProgress }}
+          />
+          <Home1 />
+          <About1 />
+          <Resume1 />
+          <Projects1 />
+          <Contact1 />
+        </Suspense>
       </div>
     </>
   );
