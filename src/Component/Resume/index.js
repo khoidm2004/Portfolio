f7b0fe72ec77319dc7a419b630f5efe33cc71c26
khoidm2004 <khoidm2004@gmail.com>
2024-11-17 00:53:40 +0200

    Create importation file

---
+++ src/Component/Resume/index.js
@@ -0,0 +1,39 @@
+import Python from "../Image/Skills/Python.png";
+import HTML from "../Image/Skills/HTML.png";
+import CSS from "../Image/Skills/CSS.png";
+import Js from "../Image/Skills/Js.png";
+import Ts from "../Image/Skills/Ts.png";
+import CPP from "../Image/Skills/CPP.png";
+import ReactLogo from "../Image/Skills/ReactLogo.png";
+import Vite from "../Image/Skills/Vite.png";
+import Node from "../Image/Skills/Node.png";
+import Selenium from "../Image/Skills/Selenium.png";
+import SQLite from "../Image/Skills/SQLite.png";
+import Git from "../Image/Skills/Git.png";
+import Mocha from "../Image/Skills/Mocha.png";
+import MySQL from "../Image/Skills/MySQL.png";
+import Jest from "../Image/Skills/Jest.png";
+import Firebase from "../Image/Skills/Firebase.png";
+import Azure from "../Image/Skills/Azure.png";
+import R_Pico from "../Image/Skills/R_Pico.png";
+
+export {
+  Python,
+  HTML,
+  CSS,
+  Js,
+  Ts,
+  CPP,
+  ReactLogo,
+  Vite,
+  Node,
+  Selenium,
+  SQLite,
+  Git,
+  Mocha,
+  MySQL,
+  Jest,
+  Firebase,
+  Azure,
+  R_Pico,
+};
