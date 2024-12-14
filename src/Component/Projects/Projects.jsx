a3b3c182ab6e18d821322baaff21577eedac7469
khoidm2004 <khoidm2004@gmail.com>
2024-12-14 08:19:23 +0200

    Added Unity project

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -62,6 +62,18 @@ export default function Projects() {
         "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",
       referrerpolicy: "strict-origin-when-cross-origin",
     },
+    {
+      type: "video",
+      description: "Brick Wall Shooting Game (C# + Unity)",
+      width: "746",
+      height: "400",
+      src: "https://www.youtube.com/embed/KbQw87Tvie8",
+      title: "Brick wall Task using c# and Unity",
+      frameborder: "0",
+      allow:
+        "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",
+      referrerpolicy: "strict-origin-when-cross-origin",
+    },
   ];
   return (
     <>
