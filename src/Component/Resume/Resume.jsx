360441117fde1aa60bc4510601c70c30c2e7656f
khoidm2004 <khoidm2004@gmail.com>
2025-11-04 20:33:55 +0200

    Update resume

--- src/Component/Resume/Resume.jsx
+++ src/Component/Resume/Resume.jsx
@@ -34,9 +34,9 @@ export default function Resume() {
             <p>
               {" "}
               Bachelor&apos;s Degree Programme in Industrial Information
-              Technology (2022 - 2026)
+              Technology (2022 - 2025)
               <br />
-              Major: Internet of Things - GPA: 4,54
+              Major: Internet of Things - GPA: 4,57
             </p>
           </div>
         </div>
@@ -46,54 +46,73 @@ export default function Resume() {
             <h2>Experience</h2>
           </div>
           <div className="Resume_rightCol">
+            {/* PC */}
+            <div>
+              <h2>
+                LAB University of Applied Sciences - Lappeenranta, Finland
+              </h2>
+              <h3>Project Coordinator</h3>
+              <p>May 2025 - Present</p>
+              <li>Designed and implemented R&D projects</li>
+              <br />
+              <li>
+                Technologies: React, Vite, TypeScript, React Native, Expo,
+                Microsoft Azure
+              </li>
+            </div>
+            <br />
+
+            {/* BE */}
+            <div>
+              <h2>Piacom - Hanoi, Vietnam</h2>
+              <h3>Back-End Developer Trainee</h3>
+              <p>May 2024 - August 2024 (4 months)</p>
+              <li>
+                Built complete backend infrastructure and service using
+                Javascript and Firebase, implementing database, authentication,
+                and image storage services for a gas station management
+                platform.
+              </li>
+              <br />
+              <li>
+                Designed efficient booking logic and reservation workflows using
+                JavaScript, reducing booking processing time by 10% and
+                improving overall user experience.
+              </li>
+              <br />
+              <li>
+                Trained on ERP systems and gas station management platforms to
+                streamline operations and optimize workflows.
+              </li>
+              <br />
+              <li>
+                Deployed serverless architecture with Vercel hosting and
+                Firebase backend, achieving 40% cost reduction.
+              </li>
+              <br />
+              <li>
+                Technologies: React, Node, JavaScript, Vite, Jest, Firebase,
+                Vercel
+              </li>
+            </div>
+            <br />
+
+            {/* FE */}
             <div>
               <h2>FPT Information System - Hanoi, Vietnam</h2>
               <h3>Front-End Developer Trainee</h3>
               <p>May 2023 - August 2023 (4 months)</p>
               <li>
-                Enhancing a car booking website by developing features,
-                optimizing code, and conducting maintenance and testing.
-                Demonstrating strong software engineering skills to ensure a
-                seamless user experience.
+                Optimized React components using Material-UI, improving page
+                load performance by 20% on a car booking platform.
               </li>
               <br />
               <li>
-                Collaborating with cross-functional teams to implement
-                responsive design principles, improving the website&apos;s
-                accessibility and ensuring a consistent user interface across
-                various devices.
+                Trained on design patterns (e.g., Component, Render Props) to
+                improve code reusability and maintainability.
               </li>
               <br />
-              <li>
-                Actively participated in code reviews, providing constructive
-                feedback and implementing best practices, contributing to the
-                overall code quality and fostering a collaborative development
-                environment.
-              </li>
-              <br />
-              <li>
-                Technologies: React, Node, Typescript, JavaScript, Material-UI,
-                Vite
-              </li>
-              <div>
-                <h2>Piacom - Hanoi, Vietnam</h2>
-                <h3>Back-End Developer Trainee</h3>
-                <p>May 2024 - August 2024 (4 months)</p>
-                <li>
-                  Developed web applications for gas station management,
-                  including inventory control, fuel tracking, and sales
-                  monitoring.
-                </li>
-                <br />
-                <li>
-                  Trained on ERP systems and gas station management platforms to
-                  streamline operations and optimize workflows.
-                </li>
-                <br />
-                <li>
-                  Technologies: React, Node, JavaScript, Vite, Jest, Firebase
-                </li>
-              </div>
+              <li>Technologies: React, Node, JavaScript, Material-UI, Vite</li>
             </div>
           </div>
         </div>
